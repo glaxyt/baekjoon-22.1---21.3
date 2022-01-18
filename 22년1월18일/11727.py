f0cf86779de0d0b4fe0edf39226eed8c421b9221
glaxyt <52828205+glaxyt@users.noreply.github.com>
2022-01-18 12:19:12 +0900

    Add files via upload

---
+++ 22년1월18일/11727.py
@@ -0,0 +1,12 @@
+# 11727번 2xn 2 타일링
+# 동적 계획법 사용 1 <= n <= 1000
+# bottom-up 방식 사용(DP 테이블)
+n = int(input())
+dp = [0] * 1001     # DP 테이블
+dp[1] = 1
+dp[2] = 3
+
+for i in range(3,1001):
+    dp[i] = dp[i-1] + 2 * dp[i-2]
+
+print(dp[n] % 10007)
