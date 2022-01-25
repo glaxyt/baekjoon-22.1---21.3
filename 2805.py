c7b157fa8ca34f2b4b79b33eff4555991f929edf
glaxyt <52828205+glaxyt@users.noreply.github.com>
2022-01-25 15:07:24 +0900

    Add files via upload

---
+++ 2805.py
@@ -0,0 +1,19 @@
+# 2805번 나무 자르기
+# 이분탐색 사용. 
+N, M = map(int, input().split())
+trees = list(map(int, input().split()))
+start = 1
+end = max(trees)
+
+while start <= end:
+    mid = (start + end) // 2
+    cnt = 0
+    for tree in trees:
+        if tree >= mid:
+            cnt += (tree - mid)
+
+    if cnt >= M:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
