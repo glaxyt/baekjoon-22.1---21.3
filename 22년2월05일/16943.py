3e1a5d37f4378151fc531fd53e3183905f41ce2d
glaxyt <52828205+glaxyt@users.noreply.github.com>
2022-02-05 17:30:10 +0900

    Update and rename 10819.py to 16943.py

--- 22년2월05일/16943.py
+++ 22년2월05일/16943.py
@@ -1,10 +1,12 @@
-# 10819번 차이를 최대로
+# 16943번 차이를 최대로
+# 파이썬 라이브러리 중에 순열이 있기에 사용
 import sys
 import itertools
 input = sys.stdin.readline
 
 a, b = input().split()
 b = int(b)
+# 순열 사용 a_nums는 이제 a로 만들 수 있는 모든 숫자가 담긴 리스트다.
 a_nums = list(map(''.join, list(itertools.permutations(a))))
 maximum = -1
 
