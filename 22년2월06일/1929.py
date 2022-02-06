dfb122b11af1c925758a256e4c7c66e1b8fa31cf
glaxyt <52828205+glaxyt@users.noreply.github.com>
2022-02-07 01:20:23 +0900

    Update 1929.py

--- 22년2월06일/1929.py
+++ 22년2월06일/1929.py
@@ -4,15 +4,18 @@ import math
 
 M, N = map(int, input().split())
 arr = [True for i in range(N+1)]
-arr[1] = 0
 
-for i in range(2, int(math.sqrt(N))+1):
-    if arr[i] == True:
-        j = 2
-        while i * j <= N:
-            arr[i*j] = False
-            j += 1
+def prime():
+    for i in range(2, int(math.sqrt(N))+1):
+        if arr[i]:
+            for j in range(i*2, N+1, i):
+                arr[j] = False
+    return [arg for arg in range(2, N+1) if arr[arg]]
 
-for num in range(M, N+1):
-    if arr[num]:
-        print(num)
+def solve(list):
+    for i in range(len(list)):
+        if M <= list[i] <= N:
+            print(list[i])
+
+prime_li = prime()
+solve(prime_li)
