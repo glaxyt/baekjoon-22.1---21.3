d2812d9199c5763d7ba26339bec2b4d5195a7c5d
glaxyt <52828205+glaxyt@users.noreply.github.com>
2022-02-07 22:18:40 +0900

    Add files via upload

---
+++ 22년2월07일/1963.py
@@ -0,0 +1,39 @@
+# 1963번 소수 경로
+# 숫자를 바꾸는 과정에서도 네자리숫자임을 유시해야하며 소수여야한다.
+from collections import deque
+
+# 1000 - 9999까지의 숫자들 중에서 소수인 것들을 뽑아내는중
+def prime(list):
+    for i in range(2, 100):
+        if list[i]:
+            for j in range(i*2, 10000, i):
+                list[j] = False
+    return list
+
+def bfs(prime, start, end):
+    queue = deque()
+    queue.append(start)
+    visited = [0] * 10000
+    while queue:
+        x = queue.popleft()
+        str_x = str(x)
+        if x == end:
+            return visited[x]
+        for i in range(4):
+            for j in range(10):
+                nx = int(str_x[:i] + str(j) + str_x[i+1:])
+                if prime[nx] == True and visited[nx] == 0 and nx >= 1000:
+                    visited[nx] = visited[x] + 1
+                    queue.append(nx)
+    print(visited[end])
+
+def solve():
+    arr = [True for _ in range(10000)]
+    prime_li = prime(arr)
+    N = int(input())
+    for _ in range(N):
+        a, b = map(int, input().split())
+        bfs(prime_li, a, b)
+
+solve()
+
