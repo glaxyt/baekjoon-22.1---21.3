9c08064f56286ce423d87a875306bcf0200d35af
glaxyt <52828205+glaxyt@users.noreply.github.com>
2022-02-07 15:02:03 +0900

    Create 1182.py

---
+++ 22년2월07일/1182.py
@@ -0,0 +1,16 @@
+# 1182번 부분수열의 합
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+N, S = map(int, input().split())
+arr = list(map(int, input().split()))
+cnt = 0
+for i in range(1, N+1):
+    comb = combinations(arr, i)
+
+    for x in comb:
+        if sum(x) == S:
+            cnt += 1
+
+print(cnt)
