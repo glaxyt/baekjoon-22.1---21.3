2d220955aa7dc41ce289e257543d610cfc74b02a
glaxyt <52828205+glaxyt@users.noreply.github.com>
2022-02-14 01:44:50 +0900

    Create 10798.py

---
+++ 22년2월14일/10798.py
@@ -0,0 +1,14 @@
+# 10798번 세로읽기
+import sys
+input = sys.stdin.readline
+graph = [[False]*15 for i in range(5)]
+
+for i in range(5):
+    word = list(input().rstrip())
+    for j in range(len(word)):
+        graph[i][j] = word[j]
+
+for i in range(15):
+    for k in range(5):
+        if graph[k][i]:
+            print(graph[k][i], end='')
